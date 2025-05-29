0435bab1301a088744dc2a1e14f342c039cd0922
Omer Šabić <omer@datalinx.si>
2025-05-29 14:04:24 +0200

    feat: set max content width to full in ListProducts page

--- src/Filament/Resources/ProductResource/Pages/ListProducts.php
+++ src/Filament/Resources/ProductResource/Pages/ListProducts.php
@@ -7,12 +7,15 @@ use Eclipse\Common\Foundation\Pages\HasScoutSearch;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 use Filament\Resources\Pages\ListRecords\Concerns\Translatable;
+use Filament\Support\Enums\MaxWidth;
 
 class ListProducts extends ListRecords
 {
     use HasScoutSearch, Translatable;
 
     protected static string $resource = ProductResource::class;
+
+    protected ?string $maxContentWidth = MaxWidth::Full->value;
 
     protected function getHeaderActions(): array
     {
