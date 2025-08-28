46fd2fefc57d6ff0b0d44c14e18d27968af6d17a
Omer Šabić <omer@datalinx.si>
2025-08-28 15:34:09 +0200

    refactor: improve plugin class

--- src/CataloguePlugin.php
+++ src/CataloguePlugin.php
@@ -2,36 +2,17 @@
 
 namespace Eclipse\Catalogue;
 
-use Filament\Contracts\Plugin;
+use Eclipse\Common\Foundation\Plugins\Plugin;
 use Filament\Panel;
 
-class CataloguePlugin implements Plugin
+class CataloguePlugin extends Plugin
 {
-    public function getId(): string
-    {
-        return 'eclipse-catalogue';
-    }
-
     public function register(Panel $panel): void
     {
-        $panel->discoverResources(__DIR__.'/Filament/Resources', 'Eclipse\\Catalogue\\Filament\\Resources');
-    }
-
-    public function boot(Panel $panel): void
-    {
-        //
-    }
-
-    public static function make(): static
-    {
-        return app(static::class);
-    }
-
-    public static function get(): static
-    {
-        /** @var static $plugin */
-        $plugin = filament(app(static::class)->getId());
-
-        return $plugin;
+        if ($panel->getId() === 'frontend') {
+            parent::register($panel);
+        } else {
+            $panel->discoverResources(__DIR__.'/Filament/Resources', 'Eclipse\\Catalogue\\Filament\\Resources');
+        }
     }
 }
