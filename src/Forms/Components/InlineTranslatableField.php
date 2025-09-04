b2075fc46b0fe96d74f1cb9bace3864d5d1f613f
Kilian Trunk <hunterteammovies@gmail.com>
2025-09-04 15:34:39 +0200

    chore: improve tabs

--- src/Forms/Components/InlineTranslatableField.php
+++ src/Forms/Components/InlineTranslatableField.php
@@ -13,6 +13,8 @@ use Filament\Forms\Components\Tabs\Tab;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Get;
 use Filament\Support\Enums\IconPosition;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\HtmlString;
 
 class InlineTranslatableField
 {
@@ -127,7 +129,10 @@ class InlineTranslatableField
                 ->label($localePrefix)
                 ->schema([$component])
                 ->icon($this->tabIconFor($fieldName))
-                ->iconPosition(IconPosition::After);
+                ->iconPosition(IconPosition::After)
+                ->extraAttributes([
+                    'class' => 'text-xs px-2 py-1.5 min-h-[2rem] font-medium',
+                ]);
         }
 
         return Group::make()
@@ -138,7 +143,10 @@ class InlineTranslatableField
                     ->dehydrated(false),
                 Tabs::make($this->name.'_tabs')
                     ->tabs($tabs)
-                    ->columnSpanFull(),
+                    ->columnSpanFull()
+                    ->extraAttributes([
+                        'class' => 'gap-1',
+                    ]),
             ])
             ->columnSpanFull();
     }
@@ -229,8 +237,19 @@ class InlineTranslatableField
     protected function tabIconFor(string $fieldName): Closure
     {
         return fn (Get $get) => $this->valueHasMeaningfulContent($get($fieldName))
-            ? 'heroicon-s-check-circle'
-            : null;
+            ? new HtmlString(Blade::render(
+                '<svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" viewBox="0 0 24 24" fill="none">
+                    <path d="m4.5 12.75 6 6 9-13.5"
+                          stroke="#22c55e" stroke-width="3"
+                          stroke-linecap="round" stroke-linejoin="round" />
+                </svg>'
+            ))
+            : new HtmlString(Blade::render(
+                '<svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 animate-pulse" viewBox="0 0 24 24" fill="none">
+                    <path d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                          stroke="#f59e0b" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>'
+            ));
     }
 
     protected function valueHasMeaningfulContent(mixed $value): bool
